f61a6126cf4d268a524b67b735ea8198786eb1f0
beez0mbie <vzhik3268@gmail.com>
2022-11-14 16:58:37 +0300

    changes after long rest

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
-import {useState} from 'react'
+import BlogList from './BlogList';
+import useFecth from './useFetch';
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsun....', author: 'spider man', id:1 },
-        {title: 'Strange Things happens', body: 'lorem ipsun....', author: 'Liza', id:2 },
-        {title: 'Cool story', body: 'lorem ipsun....', author: 'Remark', id:3 },
-    ])
+    const url = 'http://localhost:8000/blogs'
+
+    const {data: blogs, isPending, error} = useFecth(url)
+    
 
     return (
         <div className="home">
-            { blogs.map( (blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2 className="title">{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            { error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList 
+                blogs={blogs} 
+                title='All blogs' 
+            />}
         </div>
     )
 }
