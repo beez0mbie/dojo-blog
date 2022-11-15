67e9a5329f1cba1215d3e3c84bbffcd4e7632b4a
beez0mbie <vzhik3268@gmail.com>
2022-11-15 18:26:15 +0300

    add styles for create

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFecth from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
+    const {data: blog, isPending, error} = useFecth(`http://localhost:8000/blogs/${id}`);
 
     return ( 
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            { blog && (
+                <article>
+                    <h2 className="title">{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div className="body">{blog.body}</div>
+                </article>
+            ) }
         </div> 
     );
 }
