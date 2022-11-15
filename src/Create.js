6448f24c30d825f36595f432bbabab7fa18615f9
beez0mbie <vzhik3268@gmail.com>
2022-11-15 19:50:53 +0300

    Create page with form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 const Create = () => {
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('yoshi');
 
     return ( 
         <div className="create">
@@ -17,13 +19,21 @@ const Create = () => {
                 <label>Blog body:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog author</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value='mario'>mario</option>
                     <option value='yoshi'>yoshi</option>
                 </select>
                 <button>Add blog</button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
             </form>
         </div> 
     );
